da9c24c42f0e89357c6126cb75a3480f144380c5
bankoleoye <bankoleoye.s@gmail.com>
2023-04-12 16:59:34 +0100

    initial commit

---
+++ learn.py
@@ -0,0 +1,27 @@
+# from django.urls import reverse
+from datetime import date, timedelta
+
+# python = ['John Cena', 'Eric Bailey', 'Michael Jordan', 'Terry Aaya']
+# holy_grail = {'Arthur':40,'Galahad':35,'Lancelot':39}
+
+# python.sort(key = lambda name:name.split(" ")[-1])
+# print(python)
+
+# add_p = lambda a,b : a-b 
+# print(add_p(7,10))
+
+# str = " john  hsiowef ksukhfj "
+# print(str.strip())
+
+# txt = ",,,,,rrttgg.....banana....rrrppp"
+
+# x = txt.strip(",rgt.p")
+
+# print(x)
+
+count = 10**5
+nums = []
+for i in range(count):
+    nums.append(i)
+    
+print(nums.reverse())
